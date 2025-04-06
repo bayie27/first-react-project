f52c19f35288b80404fd6bb969decf1fdab9cdf3
bayie27 <danielsahagun68@gmail.com>
2025-04-06 22:51:13 +0800

    add Tailwind CSS integration and update styles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-
-
-import { useState, useEffect} from "react"
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0)
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-  
-  return (
-    <div className = "card" onClick={() => setCount((count + 1))}>
-      <h2>{title} <br/> {count || null} </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-    </div>
-  )
-}
+import React from "react";
 
 const App = () => {
   return (
-    <div className = "card-container">
-      <Card title = "Star Wars"/>
-      <Card title = "Avatar"/>
-      <Card title = "The Lion King"/>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
